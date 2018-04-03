8fd5df0e7bcff69d49c308687ec0fdbe92f48f61
Martin Wrodarczyk <martin-wr@web.de>
2018-04-04 00:07:38 +0200

    updated http request implementation

--- js/index.js
+++ js/index.js
@@ -1,33 +1,49 @@
-const colorsRYB = ["#FE2712", "#FC600A", "#66B032", "#0080FF", "#347C98", "#0247FE",
+const API_URL = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+const TWEET_URL = "https://twitter.com/intent/tweet?text=";
+const COLORS = ["#FE2712", "#FC600A", "#66B032", "#0080FF", "#347C98", "#0247FE",
  "#4424D6", "#8601AF", "#C21460", "#333333"];
-const quotes = [];
-var i = 0;
+const QUOTES = [];
+let i = 0;
 
 function fetchQuote(){
-  return fetch("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", {cache: "no-store"})
-    .then(res => res.json())
-    .then(quote => {
-      quotes.push(quote[0]);
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', API_URL + "_=" + new Date().getTime(), true);
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const quote = JSON.parse(this.responseText);
+      QUOTES.push(quote[0]);
       setQuoteUI(i);
-    })
-    .catch(err => { 
-      if (err) alert("Could not retrieve a new quote. Please check your internet connection.") 
-    });
+    } else {
+      alert("Could not retrieve a new quote. Please check your internet connection.") 
+    }
+  }
+  xhr.send();
+
+  // Alternative: fetch API
+  // fetch(API_URL, {cache: "no-store"})
+  //   .then(res => res.json())
+  //   .then(quote => {
+  //     QUOTES.push(quote[0]);
+  //     setQuoteUI(i);
+  //   })
+  //   .catch(err => { 
+  //     if (err) alert("Could not retrieve a new quote. Please check your internet connection.") 
+  //   });
 }
 
 function setQuoteUI(id){
   $('#message').fadeOut(500, function(){
-    $(this).html(quotes[id].content).fadeIn(500);
+    $(this).html(QUOTES[id].content).fadeIn(500);
   });
   $('#author').fadeOut(500, function(){
-    $(this).html(quotes[id].title).fadeIn(500);
+    $(this).html(QUOTES[id].title).fadeIn(500);
   });
   changeBackground(id);
 }
 
 function changeBackground(id){
   var html = document.getElementsByTagName('html')[0];
-  html.style.setProperty("--main-color", colorsRYB[quotes[id].ID % colorsRYB.length]);
+  html.style.setProperty("--main-color", COLORS[QUOTES[id].ID % COLORS.length]);
 }
 
 function updateUI() {
@@ -46,7 +62,7 @@ $(document).ready(function(){
   $("#rightArrow").on("click", function(event){
     if(event.detail == 1){
       i++;
-      if(i >= quotes.length - 1) fetchQuote();
+      if(i >= QUOTES.length - 1) fetchQuote();
       else setQuoteUI(i);
       updateUI();
     }
@@ -70,7 +86,7 @@ $(document).ready(function(){
       postTitle = message;
     }
     postTitle += "\n- " + author;
-    window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(postTitle),
-    "twitterwindow", height=450, width=550, toolbar=0, menubar=0, directories=0, scrollbars=0);
+    window.open(TWEET_URL + encodeURIComponent(postTitle), "twitterwindow", height=450, width=550, 
+      toolbar=0, menubar=0, directories=0, scrollbars=0);
   });
 });
